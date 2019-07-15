4c1d8db22ded9a3980245cb146a7d72538fe5968
USER <USER@AD01087533>
2019-07-15 18:36:18 +0900

    coding convention 적용

---
+++ src/main/java/com/nts/reservation/config/ApplicationConfig.java
@@ -0,0 +1,12 @@
+package com.nts.reservation.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+//@Configuration
+//@ComponentScan(basePackages = { ,  })
+//@Import({ DBConfig.class })
+//public class ApplicationConfig {
+//
+//}
---
+++ src/main/java/com/nts/reservation/config/WebMvcContextConfiguration.java
@@ -0,0 +1,38 @@
+package com.nts.reservation.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+//public class WebMvcContextConfiguration extends WebMvcConfigurerAdapter {
+//	@Override
+//	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//		registry.addResourceHandler("/assets/**").addResourceLocations("classpath:/META-INF/resources/webjars/")
+//				.setCachePeriod(31556926);
+//		registry.addResourceHandler("/css/**").addResourceLocations("/css/").setCachePeriod(31556926);
+//		registry.addResourceHandler("/img/**").addResourceLocations("/img/").setCachePeriod(31556926);
+//		registry.addResourceHandler("/js/**").addResourceLocations("/js/").setCachePeriod(31556926);
+//	}
+//
+//	// default servlet handler를 사용하게 합니다.
+//	@Override
+//	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+//		configurer.enable();
+//	}
+//
+//	@Override
+//	public void addViewControllers(final ViewControllerRegistry registry) {
+//		registry.addViewController("/").setViewName("main");
+//	}
+//
+//	@Bean
+//	public InternalResourceViewResolver getInternalResourceViewResolver() {
+//		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+//		resolver.setPrefix("/WEB-INF/htmls/");
+//		resolver.setSuffix(".html");
+//		return resolver;
+//	}
+//}
