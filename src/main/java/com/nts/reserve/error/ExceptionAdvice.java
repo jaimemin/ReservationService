144e031d66b1282c5708f3d19c9bede738ad35d1
USER <USER@AD01087533>
2019-07-26 10:37:52 +0900

    ControllerAdvice, exception.jsp 코드 수정

--- src/main/java/com/nts/reserve/error/ExceptionAdvice.java
+++ src/main/java/com/nts/reserve/error/ExceptionAdvice.java
@@ -1,7 +1,5 @@
 package com.nts.reserve.error;
 
-import java.io.IOException;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.ui.Model;
@@ -12,8 +10,11 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class ExceptionAdvice {
 	private static final Logger LOGGER = LoggerFactory.getLogger(ExceptionAdvice.class);
 	
-	@ExceptionHandler(IOException.class)
-	public String errorException(Model model, Exception exception) {
+	/*
+	 * 모든 에러를 동일하게 처리할 것이기 때문에 Exception.class
+	 */
+	@ExceptionHandler(Exception.class)
+	public String handleException(Model model, Exception exception) {
 		LOGGER.info("@ControllerAdvice 방식 \n"
 				+ "###exception: " + exception.getMessage());
 		model.addAttribute("exception", exception);
--- src/main/webapp/WEB-INF/views/error/exception.jsp
+++ src/main/webapp/WEB-INF/views/error/exception.jsp
@@ -10,5 +10,10 @@
 <body>
 	<h1>오류가 발생했습니다.</h1>
 	<h1>관리자에게 문의해주세요.</h1>
+
+	<!--  [톰캣 설치 디렉토리]/logs/에 있는 로그파일에 기록 -->
+	<c:forEach items="${exception.getStackTrace()}" var="stack">
+		application.log(${stack.toString()});
+	</c:forEach>
 </body>
 </html>
