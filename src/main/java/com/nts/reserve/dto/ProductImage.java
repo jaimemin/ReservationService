26a90998372d2a006c9d4eafacc3f678e87a015f
USER <USER@AD01087533>
2019-07-30 10:31:00 +0900

    Api 명세에 맞는 DTO 작성
    필요한 DTO 작성 완료

---
+++ src/main/java/com/nts/reserve/dto/ProductImage.java
@@ -0,0 +1,105 @@
+package com.nts.reserve.dto;
+
+import java.time.LocalDateTime;
+
+public class ProductImage {
+	private int id;
+	private int productId;
+	private String type;
+	private int fileId;
+	private String fileName;
+	private String saveFileName;
+	private String contentType;
+	private boolean deleteFlag;
+	private LocalDateTime createdDate;
+	private LocalDateTime modifiedDate;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getProductId() {
+		return productId;
+	}
+
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public int getFileId() {
+		return fileId;
+	}
+
+	public void setFileId(int fileId) {
+		this.fileId = fileId;
+	}
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+
+	public String getSaveFileName() {
+		return saveFileName;
+	}
+
+	public void setSaveFileName(String saveFileName) {
+		this.saveFileName = saveFileName;
+	}
+
+	public String getContentType() {
+		return contentType;
+	}
+
+	public void setContentType(String contentType) {
+		this.contentType = contentType;
+	}
+
+	public boolean isDeleteFlag() {
+		return deleteFlag;
+	}
+
+	public void setDeleteFlag(boolean deleteFlag) {
+		this.deleteFlag = deleteFlag;
+	}
+
+	public LocalDateTime getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(LocalDateTime createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public LocalDateTime getModifiedDate() {
+		return modifiedDate;
+	}
+
+	public void setModifiedDate(LocalDateTime modifiedDate) {
+		this.modifiedDate = modifiedDate;
+	}
+
+	@Override
+	public String toString() {
+		return "ProductImage [id=" + id + ", productId=" + productId + ", type=" + type + ", fileId=" + fileId
+				+ ", fileName=" + fileName + ", saveFileName=" + saveFileName + ", contentType=" + contentType
+				+ ", deleteFlag=" + deleteFlag + ", createdDate=" + createdDate + ", modifiedDate=" + modifiedDate
+				+ "]";
+	}
+
+}
---
+++ src/main/java/com/nts/reserve/dto/ReservationUserComment.java
@@ -0,0 +1,125 @@
+package com.nts.reserve.dto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class ReservationUserComment {
+	private String comment;
+	private int id;
+	private List<ReservationUserCommentImage> commentImages;
+	private LocalDateTime createdDate;
+	private LocalDateTime modifiedDate;
+	private int productId;
+	private LocalDateTime reservationDate;
+	private String reservationEmail;
+	private int reservationInfoId;
+	private String reservationName;
+	private String reservationTelephone;
+	private int score;
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public List<ReservationUserCommentImage> getCommentImages() {
+		return commentImages;
+	}
+
+	public void setCommentImages(List<ReservationUserCommentImage> commentImages) {
+		this.commentImages = commentImages;
+	}
+
+	public LocalDateTime getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(LocalDateTime createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public LocalDateTime getModifiedDate() {
+		return modifiedDate;
+	}
+
+	public void setModifiedDate(LocalDateTime modifiedDate) {
+		this.modifiedDate = modifiedDate;
+	}
+
+	public int getProductId() {
+		return productId;
+	}
+
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+
+	public LocalDateTime getReservationDate() {
+		return reservationDate;
+	}
+
+	public void setReservationDate(LocalDateTime reservationDate) {
+		this.reservationDate = reservationDate;
+	}
+
+	public String getReservationEmail() {
+		return reservationEmail;
+	}
+
+	public void setReservationEmail(String reservationEmail) {
+		this.reservationEmail = reservationEmail;
+	}
+
+	public int getReservationInfoId() {
+		return reservationInfoId;
+	}
+
+	public void setReservationInfoId(int reservationInfoId) {
+		this.reservationInfoId = reservationInfoId;
+	}
+
+	public String getReservationName() {
+		return reservationName;
+	}
+
+	public void setReservationName(String reservationName) {
+		this.reservationName = reservationName;
+	}
+
+	public String getReservationTelephone() {
+		return reservationTelephone;
+	}
+
+	public void setReservationTelephone(String reservationTelephone) {
+		this.reservationTelephone = reservationTelephone;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+	@Override
+	public String toString() {
+		return "Comment [comment=" + comment + ", id=" + id + ", commentImages=" + commentImages + ", createdDate="
+				+ createdDate + ", modifiedDate=" + modifiedDate + ", productId=" + productId + ", reservationDate="
+				+ reservationDate + ", reservationEmail=" + reservationEmail + ", reservationInfoId="
+				+ reservationInfoId + ", reservationName=" + reservationName + ", reservationTelephone="
+				+ reservationTelephone + ", score=" + score + "]";
+	}
+
+}
