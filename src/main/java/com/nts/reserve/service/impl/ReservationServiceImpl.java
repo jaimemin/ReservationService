622d2787f39f9db8ac181d132b969af229b2e4b3
jaimemin <jaimemin@gmail.com>
2019-08-19 12:28:08 +0900

    버그 수정
    이미지 있는 댓글 날짜 undefined로 나오는 에러 수정

--- src/main/java/com/nts/reserve/service/impl/ReservationServiceImpl.java
+++ src/main/java/com/nts/reserve/service/impl/ReservationServiceImpl.java
@@ -1,7 +1,6 @@
 package com.nts.reserve.service.impl;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
